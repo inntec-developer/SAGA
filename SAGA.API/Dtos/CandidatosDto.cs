b820d9c714b7530d7c159253b670974ff4ed4081
Pablovitch <34140568+Pablovitch@users.noreply.github.com>
2018-03-16 13:54:21 -0600

    First Commit your God
    Folders of controllers and folder of dtos

---
+++ SAGA.API/Dtos/CandidatosDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SAGA.BOL;
+using SAGA.DAL;
+
+namespace SAGA.API.Dtos
+{
+    public class CandidatosDto
+    {
+        public ICollection<Pais> Paises { get; set; }
+    }
+}
