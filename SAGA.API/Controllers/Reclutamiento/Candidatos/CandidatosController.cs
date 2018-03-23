58720b4a8e0c1ca10bb5d7388240fc42c5b8ecf1
Pablovitch <34140568+Pablovitch@users.noreply.github.com>
2018-03-23 09:34:53 -0600

    Add controller Canidatos
    Add Controller Changes

--- SAGA.API/Controllers/Reclutamiento/Candidatos/CandidatosController.cs
+++ SAGA.API/Controllers/Reclutamiento/Candidatos/CandidatosController.cs
@@ -1,14 +1,103 @@
-﻿using System;
+﻿using SAGA.DAL;
+using SAGA.BOL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using SAGA.API.Dtos;
 
 namespace SAGA.API.Controllers
 
 {
+    [RoutePrefix("api/Candidatos")]
     public class CandidatosController : ApiController
     {
+
+        private SAGADBContext db;
+
+        public CandidatosController()
+        {
+            db = new SAGADBContext();
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public IHttpActionResult GetPaises()
+        {
+            CandidatosDto Paises = new CandidatosDto();
+
+            Paises.Paises = (from pais in db.Paises
+                             //where pais.Id == 42
+                             select pais).ToList();
+
+            return Ok(Paises);
+        }
+
+        [HttpGet]
+        [Route("getestados")]
+        public IHttpActionResult GetEstados(int Pais)
+        {
+
+            CandidatosDto Estados = new CandidatosDto();
+
+            Estados.Estados = (from estado in db.Estados
+                               where estado.PaisId == Pais
+                             select estado).ToList();
+
+            return Ok(Estados);
+        }
+
+        [HttpGet]
+        [Route("getmunicipios")]
+        public IHttpActionResult GetMunicipios(int Estado)
+        {
+
+            CandidatosDto Municipios = new CandidatosDto();
+
+            Municipios.Municipios = (from municipios in db.Municipios
+                                     where municipios.EstadoId == Estado
+                                     select municipios).ToList();
+
+            return Ok(Municipios);
+        }
+
+        [HttpGet]
+        [Route("getcolonias")]
+        public IHttpActionResult GetColonias(int Municipio)
+        {
+
+            CandidatosDto Colonias = new CandidatosDto();
+
+            Colonias.Colonias = (from colonias in db.Colonias
+                                 where colonias.MunicipioId == Municipio
+                                 select colonias).ToList();
+
+            return Ok(Colonias);
+        }
+
+        [HttpGet]
+        [Route("getcandidatos")]
+        public IHttpActionResult GetCandidatos()
+        {
+
+            CandidatosDto Candidatos = new CandidatosDto();
+
+            Candidatos.Candidatos = (from candidatos in db.Candidatos
+
+                                     select new CandidatosGralDto
+                                     {
+                                         Id = candidatos.Id,
+                                         Candidato = candidatos.CodigoPostal,
+                                         CP = candidatos.CodigoPostal,
+                                         Curp = candidatos.CURP,
+                                         Rfc = candidatos.RFC,
+                                         Nss = candidatos.NSS
+                                      }).ToList();
+
+            return Ok(Candidatos.Candidatos);
+        }
+
     }
 }
