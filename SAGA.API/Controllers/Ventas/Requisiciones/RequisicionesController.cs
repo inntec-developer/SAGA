37543dafe760744328f80ff632d3b197b51d30bb
Nestor Iniguez <nextor.ivan@icloud.com>
2018-03-25 20:06:24 -0600

    Add function Get Dafmo
    Add fucntion Get DAMFO

--- SAGA.API/Controllers/Ventas/Requisiciones/RequisicionesController.cs
+++ SAGA.API/Controllers/Ventas/Requisiciones/RequisicionesController.cs
@@ -1,5 +1,6 @@
 ﻿using SAGA.DAL;
 using SAGA.BOL;
+using SAGA.API.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,8 +23,30 @@ namespace SAGA.API.Controllers
         [HttpGet]
         [Route("getDamfos")]
         public IHttpActionResult Get()
-      {
-            return Ok(db.DAMFO290.ToList());
+        {
+            Damfo290Dto DamfoDto = new Damfo290Dto();
+
+            DamfoDto.Damfo290 = (from damfo in db.DAMFO290
+                        join cliente in db.Clientes on damfo.ClienteId equals cliente.Id
+                        join giroEmpresa in db.GirosEmpresas on cliente.GiroEmpresaId equals giroEmpresa.Id
+                        join actividadEmpresa in db.ActividadesEmpresas on giroEmpresa.Id equals actividadEmpresa.Id
+                        join tipoReclutamiento in db.TiposReclutamientos on damfo.TipoReclutamientoId equals tipoReclutamiento.Id
+                        join claseReclutamiento in db.ClasesReclutamientos on damfo.ClaseReclutamientoId equals claseReclutamiento.Id
+                        select new Damfo290GralDto {
+                            Id = damfo.Id,
+                            Cliente = cliente.RazonSocial,
+                            NombrePerfil = damfo.NombrePerfil,
+                            GiroEmpresa = giroEmpresa.giroEmpresa,
+                            ActividadEmpresa = actividadEmpresa.actividadEmpresa,
+                            TipoReclutamiento = tipoReclutamiento.tipoReclutamiento,
+                            ClaseReclutamiento = claseReclutamiento.clasesReclutamiento,
+                            SueldoMinimo = damfo.SueldoMinimo,
+                            SueldoMaximo = damfo.SueldoMaximo,
+                            fch_Creacion = damfo.fch_Creacion
+                       }).ToList();
+
+
+            return Ok(DamfoDto.Damfo290);
         }
     }
 }
--- SAGA.API/Dtos/Reclutamiento/DAMFO/Damfo290Dto.cs
+++ SAGA.API/Dtos/Reclutamiento/DAMFO/Damfo290Dto.cs
@@ -7,14 +7,6 @@ namespace SAGA.API.Dtos
 {
     public class Damfo290Dto
     {
-        public Guid Id { get; set; }
-        public string NombrePerfil { get; set; }
-        public string GieroEmpresa { get; set; }
-        public string ActividadEmpresa { get; set; }
-        public string TipoReclutamiento { get; set; }
-        public string ClaseReclutamiento { get; set; }
-        public decimal SueldoMinimo { get; set; }
-        public decimal SueldoMaximo { get; set; }
-        public DateTime fch_Creacion { get; set; }
+        public ICollection<Damfo290GralDto> Damfo290 { get; set; }
     }
 }
