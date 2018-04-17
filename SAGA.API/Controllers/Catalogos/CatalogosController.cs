55dbd2c55284624b38375e78f1d03ea9bddc3a80
Nestor Iniguez <nextor.ivan@icloud.com>
2018-04-17 13:40:42 -0500

    Add Controller
    Add controller CatalogosContelller.cs

---
+++ SAGA.API/Controllers/Catalogos/CatalogosController.cs
@@ -0,0 +1,51 @@
+﻿using SAGA.DAL;
+using SAGA.BOL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SAGA.API.Controllers
+{
+    [RoutePrefix("api/Catalogos")]
+    public class CatalogosController : ApiController
+    {
+        private SAGADBContext db;
+        public CatalogosController()
+        {
+            db = new SAGADBContext();
+        }
+
+        [HttpGet]
+        [Route("getDocDamsa")]
+        public IHttpActionResult GetDocDamsa()
+        {
+            try
+            {
+                var documentosDamsa = db.DocumentosDamsa.ToList();
+                return Ok(documentosDamsa);
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("getPrestacionesLey")]
+        public IHttpActionResult GetPrestacionesLey()
+        {
+            try
+            {
+                var prestacionesLey = db.PrestacionesLey.ToList();
+                return Ok(prestacionesLey);
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
+    }
+}
