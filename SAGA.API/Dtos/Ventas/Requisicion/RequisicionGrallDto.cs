1cf209f08386c8e4da47796e9c4162dc0061ec12
Nestor Iniguez <nextor.ivan@icloud.com>
2018-04-18 13:34:14 -0500

    Add Class

---
+++ SAGA.API/Dtos/Ventas/Requisicion/RequisicionGrallDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAGA.API.Dtos.Ventas.Requisicion
+{
+    public class RequisicionGrallDto
+    {
+        public Guid Id { get; set; }
+        public string Cliente { get; set; }
+        public string NombrePerfil { get; set; }
+        public string GiroEmpresa { get; set; }
+        public string ActividadEmpresa { get; set; }
+        public string TipoReclutamiento { get; set; }
+        public string ClaseReclutamiento { get; set; }
+        public decimal SueldoMinimo { get; set; }
+        public decimal SueldoMaximo { get; set; }
+        public DateTime fch_Creacion { get; set; }
+        public DateTime? fch_Cumplimiento { get; set; }
+    }
+}
