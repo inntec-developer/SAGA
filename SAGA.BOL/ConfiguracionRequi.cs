51bc9868c21ccb885257adb7c4e6f181a03bb81e
Nestor Iniguez <nextor.ivan@icloud.com>
2018-07-04 16:19:59 -0500

    Change ConfiguracionRequi

--- SAGA.BOL/ConfiguracionRequi.cs
+++ SAGA.BOL/ConfiguracionRequi.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,8 @@ namespace SAGA.BOL
 {
     public class ConfiguracionRequi
     {
-        public int id { get; set; }
+        [Key]
+        public Guid id { get; set; }
         public Guid IdRequi { get; set; }
         public int IdEstructura { get; set; }
         public string Campo { get; set; }
