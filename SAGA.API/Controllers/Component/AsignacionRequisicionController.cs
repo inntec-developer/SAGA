a9d550e6d8a914e2a5d717ae58d9591134969ca1
Nestor Iniguez <nextor.ivan@icloud.com>
2018-08-20 15:09:36 -0500

    cambios en controlador

--- SAGA.API/Controllers/Component/AsignacionRequisicionController.cs
+++ SAGA.API/Controllers/Component/AsignacionRequisicionController.cs
@@ -23,19 +23,52 @@ namespace SAGA.API.Controllers.Component
         [HttpGet]
         [Route("getUserGroup")]
         public IHttpActionResult GetUserGroup()
-
+   
         {
+            var rol = db.Privilegios.Where(p => p.EstructuraId.Equals(15)).Select(x => x.RolId).ToList();
+            var entidad = db.RolEntidades.Where(x => rol.Contains(x.RolId)).Select(x => x.EntidadId).ToList();
+
             var asignacion = db.Usuarios
-                .OrderBy(u => u.Nombre)
-                .Where(u => u.Activo.Equals(true))
+                .Where(u => u.Activo.Equals(true) && u.TipoUsuarioId <= 5 && entidad.Contains(u.Id))
                 .Select(x => new AsigancionDto
                 {
                     Id = x.Id,
-                    Nombre = x.Nombre + " " + x.ApellidoMaterno,
+                    Nombre = x.Nombre + " " + x.ApellidoPaterno + " " + x.ApellidoMaterno,
                     Descripcion = x.Usuario,
                     Email = db.Emails.Where(e => e.EntidadId.Equals(x.Id)).Select(e => e.email).FirstOrDefault(),
                     TipoEntidadId = x.TipoEntidadId
-                })
+                }).OrderBy(u => u.Nombre)
+                .Union(
+                    db.Grupos
+                    .OrderByDescending(g => g.Nombre)
+                    .Where(g => g.Activo.Equals(true) && entidad.Contains(g.Id))
+                    .Select(g => new AsigancionDto
+                    {
+                        Id = g.Id,
+                        Nombre = g.Nombre + " " + g.ApellidoPaterno + " " + g.ApellidoMaterno,
+                        Descripcion = g.Descripcion,
+                        Email = db.Emails.Where(e => e.EntidadId.Equals(g.Id)).Select(e => e.email).FirstOrDefault(),
+                        TipoEntidadId = g.TipoEntidadId
+                    })
+                ).OrderBy( g=> g.Nombre);
+            return Ok(asignacion);
+        }
+
+        [HttpGet]
+        [Route("getUserGroupR")]
+        public IHttpActionResult GetUserGroupR()
+
+        {
+            var asignacion = db.Usuarios
+                .Where(u => u.Activo.Equals(true) && u.TipoUsuarioId <= 6)
+                .Select(x => new AsigancionDto
+                {
+                    Id = x.Id,
+                    Nombre = x.Nombre + " " + x.ApellidoPaterno,
+                    Descripcion = x.Usuario,
+                    Email = db.Emails.Where(e => e.EntidadId.Equals(x.Id)).Select(e => e.email).FirstOrDefault(),
+                    TipoEntidadId = x.TipoEntidadId
+                }).OrderBy(u => u.Nombre)
                 .Union(
                     db.Grupos
                     .OrderByDescending(g => g.Nombre)
@@ -43,12 +76,12 @@ namespace SAGA.API.Controllers.Component
                     .Select(g => new AsigancionDto
                     {
                         Id = g.Id,
-                        Nombre = g.Nombre + " " + g.ApellidoMaterno,
+                        Nombre = g.Nombre + " " + g.ApellidoPaterno,
                         Descripcion = g.Descripcion,
                         Email = db.Emails.Where(e => e.EntidadId.Equals(g.Id)).Select(e => e.email).FirstOrDefault(),
                         TipoEntidadId = g.TipoEntidadId
                     })
-                ).OrderByDescending( g=> g.TipoEntidadId);
+                ).OrderBy(g => g.Nombre);
             return Ok(asignacion);
         }
 
