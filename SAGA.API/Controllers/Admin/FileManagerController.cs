199ec923812cbb141e0875d4b324beb5dc07ebed
MelinaMorales <melina.morales.c@gmail.com>
2018-08-24 09:28:19 -0500

    Melina24082018

--- SAGA.API/Controllers/Admin/FileManagerController.cs
+++ SAGA.API/Controllers/Admin/FileManagerController.cs
@@ -1,35 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace SAGA.API.Controllers
 {
+
     [RoutePrefix("api/admin")]
     public class FileManagerController : ApiController
     {
+       
         [HttpGet]
         [Route("getFiles")]
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
         public IHttpActionResult GetFiles()
         {
             var path = "~/utilerias/";
             string fullPath = System.Web.Hosting.HostingEnvironment.MapPath(path);
             DirectoryInfo folderInfo = new DirectoryInfo(fullPath);
             List<string> extensions = folderInfo.GetFiles("*.*", SearchOption.TopDirectoryOnly).Select(x => x.FullName).ToList();
-            //var mocos = folderInfo.GetFiles("*.*", SearchOption.TopDirectoryOnly).Select(x => new {
-            //    fullPath = x.FullName,
-            //    nom = x.Name,
-            //    ext = x.Extension,
-            //    size = (long)x.Length / 1024,
-            //    fc = x.LastWriteTime.ToShortDateString()
-            //});
+
 
             var files = folderInfo.GetFiles(
                     "*.*",
-                    SearchOption.AllDirectories).Select(x => new {
+                    SearchOption.AllDirectories).Select(x => new
+                    {
                         fullPath = x.FullName,
                         nom = x.Name,
                         ext = x.Extension,
@@ -46,17 +50,84 @@ namespace SAGA.API.Controllers
                          Count = extCnt.Count()
                      });
 
-            //can read and compare for given extension
-
-            //foreach (var extension in extensions)
-            //{
-            //    var files = folderInfo.GetFiles("*.*", SearchOption.TopDirectoryOnly).Where(x => x.Extension == extension).ToList();
-            //    Console.WriteLine("Total Files with {0} Extension = {1}  ", extension, files.Count);
-            //   
-            //}
-
-        
             return Ok(files);
+        }
+
+        [HttpGet]
+        [Route("downloadFiles")]
+        public IHttpActionResult DownloadFiles(string file)
+        {
+            try
+            {
+                string localFilePath = System.Web.Hosting.HostingEnvironment.MapPath(file);
+
+                if (File.Exists(localFilePath))
+                {
+                    string nom = Path.GetFileName(localFilePath);
+
+                    string userProfileFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                    string DownloadsFolder = userProfileFolder + "\\Downloads\\";
+                    var ruta_descarga = Path.Combine(DownloadsFolder, nom);
+
+                    WebClient wc = new WebClient();
+                    wc.DownloadFile(localFilePath, ruta_descarga);
+
+                    return Ok(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Ok(HttpStatusCode.BadRequest);
+                }
+
+            }
+            catch( Exception ex)
+            {
+                return Ok(HttpStatusCode.ExpectationFailed);
+            }
+
+          
+        }
+
+        [HttpPost]
+        [Route("UploadFile")]
+        public IHttpActionResult UploadImage()
+        {
+            string fileName = null;
+            var path = "";
+            try
+            {
+                var httpRequest = HttpContext.Current.Request;
+                var postedFile = httpRequest.Files["file"];
+                var id = Guid.Parse(Path.GetFileNameWithoutExtension(postedFile.FileName).ToString());
+                //var id = new string(Path.GetFileNameWithoutExtension(postedFile.FileName).Take(10).ToArray()).Replace(" ", "-");
+
+                var ext = Path.GetExtension(postedFile.FileName);
+                fileName = Path.GetFileName(postedFile.FileName);
+
+                if (ext == ".jpg" || ext == ".jpeg" || ext == ".png")
+                {
+                    path = "~/utilerias/img/user/" + fileName;
+                }
+                else
+                {
+                    path = "~/utilerias/pdf/" + fileName;
+                }
+
+                string fullPath = System.Web.Hosting.HostingEnvironment.MapPath(path);
+
+                if (File.Exists(fullPath))
+                    File.Delete(fullPath);
+
+                postedFile.SaveAs(fullPath);
+
+                return Ok(HttpStatusCode.Created); //201
+
+            }
+            catch (Exception ex)
+            {
+                return Ok(HttpStatusCode.InternalServerError);
+            }
+
         }
 
         [HttpGet]
