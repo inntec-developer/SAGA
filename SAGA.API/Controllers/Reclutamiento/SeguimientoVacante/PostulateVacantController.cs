6361a2f6aea86635bb5d89448578c31685c0f7eb
MelinaMorales <melina.morales.c@gmail.com>
2018-09-06 09:16:30 -0500

    Melina06092018

--- SAGA.API/Controllers/Reclutamiento/SeguimientoVacante/PostulateVacantController.cs
+++ SAGA.API/Controllers/Reclutamiento/SeguimientoVacante/PostulateVacantController.cs
@@ -1,4 +1,5 @@
-﻿using SAGA.DAL;
+﻿using SAGA.API.Dtos.Reclutamiento.Seguimientovacantes;
+using SAGA.DAL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,6 +36,47 @@ namespace SAGA.API.Controllers
             }).ToList();
             return Ok(candidatos);
         }
+
+        [HttpGet]
+        [Route("getProceso")]
+        public IHttpActionResult GetProceso(Guid VacanteId)
+        {
+            var postulate = db.ProcesoCandidatos.Where(x => x.RequisicionId.Equals(VacanteId)).Select(c => c.CandidatoId).ToList();
+
+            var candidatos = db.PerfilCandidato.Where(x => postulate.Contains(x.CandidatoId)).Select(x => new {
+                candidatoId = x.CandidatoId,
+                nombre = x.Candidato.Nombre + " " + x.Candidato.ApellidoPaterno + " " + x.Candidato.ApellidoMaterno,
+                AreaExp = x.AboutMe.Select(ae => ae.AreaExperiencia.areaExperiencia).FirstOrDefault(),
+                AreaInt = x.AboutMe.Select(ai => ai.AreaInteres.areaInteres).FirstOrDefault() != null ? x.AboutMe.Select(ai => ai.AreaInteres.areaInteres).FirstOrDefault() : "",
+                localidad = x.Candidato.direcciones.Select(d => d.Municipio.municipio).FirstOrDefault() + " / " + x.Candidato.direcciones.Select(d => d.Estado.estado).FirstOrDefault(),
+                sueldoMinimo = x.AboutMe.Select(s => s.SalarioAceptable).FirstOrDefault(),
+                edad = x.Candidato.FechaNacimiento,
+                rfc = x.Candidato.RFC != null ? x.Candidato.RFC : "",
+                curp = x.Candidato.CURP,
+                //estatus = postulate.Select(d => d.Estatus)
+            }).ToList();
+            return Ok(candidatos);
+        }
+
+        [HttpPost]
+        [Route("updateStatus")]
+        public IHttpActionResult UpdateStatus(ProcesoDto datos)
+        {
+            try
+            {
+                var c = db.ProcesoCandidatos.Find(datos.CandidatoId);
+                db.Entry(c).State = System.Data.Entity.EntityState.Modified;
+                c.EstatusId = datos.EstatusId;
+
+                return Ok(HttpStatusCode.Created);
+            }
+            catch(Exception ex)
+            {
+                return Ok(HttpStatusCode.ExpectationFailed);
+            }
+            
+        }
+
 
     }
 }
---
+++ SAGA.API/Dtos/Reclutamiento/Seguimientovacantes/ProcesoDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAGA.API.Dtos.Reclutamiento.Seguimientovacantes
+{
+    public class ProcesoDto
+    {
+        public Guid CandidatoId { get; set; }
+        public int EstatusId { get; set; }
+    }
+}
