d1a67831ebb3136205cd80d09131bb1370629747
MelinaMorales <melina.morales.c@gmail.com>
2018-10-30 16:48:40 -0600

    Update SAGA.BOL/ContratadosInfo.cs

---
+++ SAGA.BOL/ContratadosInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SAGA.BOL
+{
+    public class ContratadosInfo
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid EntidadId { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string CURP { get; set; }
+        public string RFC { get; set; }
+        [Display(Name = "Número de Seguro Social")]
+        public string NSS { get; set; }
+
+        public int? PaisNacimientoId { get; set; }
+        public virtual Pais paisNacimiento { get; set; }
+
+        public int? EstadoNacimientoId { get; set; }
+        public virtual Estado estadoNacimiento { get; set; }
+
+        public int? MunicipioNacimientoId { get; set; }
+        public virtual Municipio municipioNacimiento { get; set; }
+
+        public byte GeneroId { get; set; }
+        public virtual Genero Genero { get; set; }
+
+        public Guid ReclutadorId { get; set; }
+        public DateTime fch_Creacion { get; set; }
+        public DateTime fch_Modificacion { get; set; }
+
+
+    }
+}
