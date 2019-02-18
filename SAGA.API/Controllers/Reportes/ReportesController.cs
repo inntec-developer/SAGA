bb857d98459c638533fc57aa6fdeab02578ffc8d
David <emir265@hotmail.com>
2019-02-18 16:50:58 -0600

    Reporte
    Se agrego el controlador de reporte

---
+++ SAGA.API/Controllers/Reportes/ReportesController.cs
@@ -0,0 +1,24 @@
+﻿using SAGA.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SAGA.API.Controllers.Reportes
+{
+    public class ReportesController : ApiController
+    {
+        private SAGADBContext db;
+
+
+        [HttpGet]
+        [Route("get")]
+        public IHttpActionResult Get()
+        {
+            var datos = db.Requisiciones.ToList();
+            return Ok("");
+        }
+    }
+}
