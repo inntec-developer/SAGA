94ee08762f8b3beea33e0feaee92fa76b0b97277
David <emir265@hotmail.com>
2019-03-13 12:40:52 -0600

    preguntas
    se terminaron la validaciones de los servicios

--- SAGA.API/Controllers/Catalogos/PreguntasFrecuenteController.cs
+++ SAGA.API/Controllers/Catalogos/PreguntasFrecuenteController.cs
@@ -12,14 +12,92 @@ namespace SAGA.API.Controllers
     [RoutePrefix("api/PreguntasFrecuente")]
     public class PreguntasFrecuenteController : ApiController
     {
-        private SAGADBContext db;
+        private SAGADBContext db = new SAGADBContext();
+        
+
+        [HttpGet]
+        [Route("preguntas")]
+        public IHttpActionResult preguntas()
+        {
+            try
+            {
+                var datos = db.PreguntasFrecuentes.OrderByDescending(e=>e.Id).ToList();
+              //  var datos = db.PreguntasFrecuentes.Select(e => new { e.Id, e.Pregunta, e.Respuesta, e.Activo }).ToList();
+                return Ok(datos);
+            }
+            catch (Exception ex)
+            {
+                var mensaje = ex;               
+            }
+
+            return Ok("");
+        }
 
 
         [HttpGet]
-        [Route("get")]
-        public IHttpActionResult Get()
+        [Route("add")]
+        public IHttpActionResult agregar(string pregunta, string repuesta)
         {
-            return Ok("");
+            try
+            {
+                var datos = db.PreguntasFrecuentes.ToList();
+                var pren = new PreguntasFrecuente();
+           //     pren.Id = datos.Count + 1;
+                pren.Pregunta = pregunta;
+                pren.Respuesta = repuesta;
+                pren.Activo = true;
+                db.PreguntasFrecuentes.Add(pren);
+                db.SaveChanges();
+
+                return Ok("Éxito al agregar la pregunta");
+            }
+            catch (Exception ex)
+            {
+                var mensaje = ex;
+            }
+            return Ok("Algo paso, intentelo mas tarde");
+        }
+
+        [HttpGet]
+        [Route("alter")]
+        public IHttpActionResult editar(string id, string pregunta, string repuesta, string activo)
+        {
+
+            try
+            {
+                int iden = Convert.ToInt32(id);
+                var datos = db.PreguntasFrecuentes.Where(e=>e.Id == iden).FirstOrDefault();
+                datos.Pregunta = pregunta;
+                datos.Respuesta = repuesta;
+                datos.Activo = Convert.ToBoolean(activo);
+                db.SaveChanges();
+
+                return Ok("Éxito al guardar cambios");
+            }
+            catch (Exception ex)
+            {
+                var mensaje = ex;
+            }
+            return Ok("Algo paso, intentelo mas tarde");
+        }
+
+        [HttpGet]
+        [Route("delete")]
+        public IHttpActionResult eliminar(string id)
+        {
+            try
+            {
+                int iden = Convert.ToInt32(id);
+                var datos = db.PreguntasFrecuentes.Where(e => e.Id == iden).FirstOrDefault();
+                db.PreguntasFrecuentes.Remove(datos);
+                db.SaveChanges();
+                return Ok("éxito al eliminar la pregunta");
+            }
+            catch (Exception ex)
+            {
+                var mensaje = ex;
+            }
+            return Ok("Algo paso intentelo mas tarde");
         }
     }
 }
