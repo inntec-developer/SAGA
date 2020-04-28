5302f92ea489fb0662552493482ea1b0396b31ac
Melina Morales <melina.morales.c@gmail.com>
2020-04-28 09:45:58 -0500

    Create APISAGALog.cs

---
+++ SAGA.API/Utilerias/APISAGALog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SAGA.API.Utilerias
+{
+    public class APISAGALog
+    {
+        public void WriteError(string error)
+        {
+            string path = "APISAGALog_" + DateTime.Now.ToString("ddMMyyyy") + ".txt";
+            string tempPath = GetTempPath();
+            if (!File.Exists(tempPath + path))
+            {
+                File.Create(tempPath + path);
+            }
+
+           StreamWriter stwriter = System.IO.File.AppendText(
+               tempPath + path);
+            try
+            {
+                string logLine = String.Format(
+                    "{0:G}: {1}.", DateTime.Now, error);
+                stwriter.WriteLine(logLine);
+            }
+            finally
+            {
+                stwriter.Close();
+            }
+
+        }
+        public string GetTempPath()
+        {
+            string path = System.Environment.GetEnvironmentVariable("TEMP");
+            if (!path.EndsWith("\\")) path += "\\";
+            string fullPath = System.Web.Hosting.HostingEnvironment.MapPath("~/Utilerias/");
+            return fullPath;
+        }
+    }
+}
